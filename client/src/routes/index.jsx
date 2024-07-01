6a6e9402c913a6d6bcbd87d9ca7357379f67abdf
Phan Thanh Hóa <hptprobook@gmail.com>
2024-07-01 22:43:10 +0700

    Init project

---
+++ client/src/routes/index.jsx
@@ -0,0 +1,7 @@
+import { useRoutes } from 'react-router-dom';
+
+// ==============================|| ROUTING RENDER ||============================== //
+
+export default function Routes() {
+  return useRoutes();
+}
---
+++ server/src/utils/schema.js
@@ -0,0 +1,3 @@
+/* eslint-disable comma-dangle */
+import Joi from 'joi';
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators';
