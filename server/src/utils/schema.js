ffa1d4439c516eec51215114d04a790fc7a7db49
ccxtncntus <ccxtncn00@gmail.com>
2024-07-13 23:03:25 +0700

    user

--- server/src/utils/schema.js
+++ server/src/utils/schema.js
@@ -3,92 +3,66 @@ import Joi from 'joi';
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators';
 
 export const SAVE_USER_SCHEMA = Joi.object({
-  firstName: Joi.string().min(3).max(30).required(),
-  lastName: Joi.string().min(2).max(30).required(),
-  email: Joi.string().email().required(),
   //   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  name: Joi.object({
+    firstName: Joi.string().min(1).max(30).required(),
+    lastName: Joi.string().min(1).max(30).required(),
+  }),
+  email: Joi.string().email().required(),
   passWord: Joi.string().required(),
   otp: Joi.string(),
-  getNotify: Joi.boolean().default(false),
-
-  phoneNumber: Joi.string()
+  addresses: Joi.array().items(
+    Joi.object({
+      name: Joi.string().max(50),
+      homeNumber: Joi.string().max(50),
+      street: Joi.string(),
+      ward: Joi.string(),
+      district: Joi.string(),
+      province: Joi.string(),
+      isDefault: Joi.boolean().default(false),
+      note: Joi.string(),
+    })
+  ),
+  phone: Joi.string()
     .pattern(/^[0-9]+$/)
     .min(10)
     .max(15)
     .default(null),
-  addresses: Joi.array().items(
-    Joi.object({
-      type: Joi.string().valid('home', 'work', 'other').default('home'),
-      fullName: Joi.string().max(50),
-      commune: Joi.string(),
-      district: Joi.string(),
-      province: Joi.string(),
-      detail: Joi.string(),
-      isDefault: Joi.boolean().default(false),
-    })
-  ),
-  role: Joi.string().valid('user', 'admin', 'ban').default('user'),
-  avatar: Joi.string().default(null),
-  dateOfBirth: Joi.date().max('now').default(null),
-  gender: Joi.string().valid('male', 'female', 'other').default('other'),
-  favoriteProducts: Joi.array().items(
-    Joi.object({
-      product_id: Joi.string(),
-      price_id: Joi.string(),
-    })
-  ),
-  cart: Joi.array().items(
-    Joi.object({
-      product_id: Joi.string(),
-      price_id: Joi.string(),
-      quantity: Joi.number().integer().min(1),
-    })
-  ),
-  // lastLogin: Joi.date(),
+  role: Joi.string().valid('root', 'employee', 'user', 'ban').default('user'),
+  allowNotifies: Joi.boolean().default(false),
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
+  updatedAt: Joi.date().timestamp('javascript').default(Date.now),
 });
 
 export const UPDATE_USER = Joi.object({
-  username: Joi.string().min(3).max(30),
-  // email: Joi.string().email().required(),
-  //   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  name: Joi.object({
+    firstName: Joi.string().min(1).max(30),
+    lastName: Joi.string().min(1).max(30),
+  }),
   passWord: Joi.string(),
   otp: Joi.string(),
-  phoneNumber: Joi.string()
+
+  addresses: Joi.array().items(
+    Joi.object({
+      name: Joi.string().max(50),
+      homeNumber: Joi.string().max(50),
+      street: Joi.string(),
+      ward: Joi.string(),
+      district: Joi.string(),
+      province: Joi.string(),
+      isDefault: Joi.boolean().default(false),
+      note: Joi.string(),
+    })
+  ),
+  phone: Joi.string()
     .pattern(/^[0-9]+$/)
     .min(10)
     .max(15)
     .default(null),
-  addresses: Joi.array().items(
-    Joi.object({
-      type: Joi.string().valid('home', 'work', 'other').default('home'),
-      fullName: Joi.string().max(50),
-      commune: Joi.string(),
-      district: Joi.string(),
-      province: Joi.string(),
-      detail: Joi.string(),
-      isDefault: Joi.boolean().default(false),
-    })
-  ),
-  role: Joi.string().valid('user', 'admin', 'ban').default('user'),
-  avatar: Joi.string().default(null),
-  dateOfBirth: Joi.date().max('now').default(null),
-  gender: Joi.string().valid('male', 'female', 'other').default('other'),
-  favoriteProducts: Joi.array().items(
-    Joi.object({
-      product_id: Joi.string(),
-      // price_id: Joi.string(),
-    })
-  ),
-  cart: Joi.array().items(
-    Joi.object({
-      product_id: Joi.string(),
-      price_id: Joi.string(),
-      quantity: Joi.number().integer().min(1),
-    })
-  ),
-  // lastLogin: Joi.date(),
+  role: Joi.string().valid('root', 'employee', 'user', 'ban').default('user'),
+  allowNotifies: Joi.boolean().default(false),
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
+  updatedAt: Joi.date().timestamp('javascript').default(Date.now),
 });
 
 export const SAVE_CATEGORY_SCHEMA = Joi.object({
