80c32719d5e1cafe19a9b559f68aad57456c44d3
Nguyễn Văn Tín <126067526+nvtin1104@users.noreply.github.com>
2024-07-15 19:48:55 +0700

    redux/axios

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,23 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from './hooks/useAuth';
+// eslint-disable-next-line import/order
+import { Provider } from 'react-redux';
 
 import App from './app';
-
+import { store } from './redux/store';
 // ----------------------------------------------------------------------
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <HelmetProvider>
-    <BrowserRouter>
-      <Suspense>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </Suspense>
-    </BrowserRouter>
-  </HelmetProvider>
+  <Provider store={store}>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Suspense>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </Suspense>
+      </BrowserRouter>
+    </HelmetProvider>
+  </Provider>
 );
