db723ba6637244321da1ac11833bedda159b88a5
beovan <truongvangiap04@gmail.com>
2024-07-15 20:49:34 +0700

    chore: Add categories slice to redux store

--- dashboard/src/redux/store.js
+++ dashboard/src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "./slices/userSlice";
 import productsReducer from "./slices/productSlice";
+import categoriesReducer from "./slices/categoriesSlice";
 
 export const store = configureStore({
     reducer: {
         users: userReducer,
         products: productsReducer,
+        categories: categoriesReducer,
     },
 });
