1de3d6777db80e24486b636d057b397d6eabd9dc
beovan <truongvangiap04@gmail.com>
2024-07-15 20:39:46 +0700

    update product redux

--- dashboard/src/redux/store.js
+++ dashboard/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "./slices/userSlice";
+import productsReducer from "./slices/productSlice";
 
 export const store = configureStore({
     reducer: {
         users: userReducer,
+        products: productsReducer,
     },
 });
