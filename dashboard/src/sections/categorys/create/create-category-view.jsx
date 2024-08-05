1b44a50a4c526b601ccd8f724368d89b5a8554c1
beovan <truongvangiap04@gmail.com>
2024-08-05 12:15:04 +0700

    update form category with toast

--- dashboard/src/sections/categorys/create/create-category-view.jsx
+++ dashboard/src/sections/categorys/create/create-category-view.jsx
@@ -65,101 +65,108 @@ const CreateCategoryView = () => {
 
   return (
     <Container>
-    <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-      <Typography variant="h4">Tạo một Danh mục mới</Typography>
-    </Stack>
-    <Box justifyContent="center">
-      <Box sx={{ maxWidth: 'lg', p: 6, bgcolor: 'background.paper', color: 'text.primary', borderRadius: 2, boxShadow: 3, mt: 5 }}>
-        <Typography variant="h6" gutterBottom>Chi tiết</Typography>
-        <Typography variant="body2" color="text.secondary" gutterBottom>Tiêu đề, mô tả, hình ảnh...</Typography>
-        <Formik
-          initialValues={{ productName: '', description: '', parentCategory: '' }}
-          validationSchema={validationSchema}
-          onSubmit={(values) => {
-            const data = {
-              name: values.productName,
-              description: values.description,
-              slug: values.productName.toLowerCase().replace(/ /g, '-'),
-              parentId: values.parentCategory || null || "null",
-            };
-            console.log(data);
-            dispatch(createCategoryAsync({ data }));
-          }}
-        >
-          {({ handleSubmit }) => (
-            <Form onSubmit={handleSubmit}>
-              <Grid >
-                <Grid item xs={12} sm={6}>
-                  <Field
-                    as={TextField}
-                    fullWidth
-                    id="productName"
-                    name="productName"
-                    label="Tên Danh mục"
-                    variant="outlined"
-                    placeholder="Tên danh mục"
-                    helperText={<ErrorMessage name="productName" component="div" className="error-message" />}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <InputLabel htmlFor="description">Mô tả</InputLabel>
-                  <Field name="description">
-                    {({ field, form }) => (
-                      <EditorContent
-                        {...field}
-                        value={field.value}
-                        onChange={(content) => {
-                          form.setFieldValue(field.name, content);
-                        }}
-                      />
-                    )}
-                  </Field>
-                  <FormHelperText>
-                    <ErrorMessage name="description" component="div" className="error-message" />
-                  </FormHelperText>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <InputLabel htmlFor="parentCategory">Danh mục cha</InputLabel>
-                  <FormControl fullWidth variant="outlined">
-                    <InputLabel htmlFor="parentCategory">Danh mục cha</InputLabel>
-                    <Select
-                      labelId="parentCategory-label"
-                      id="parentCategory"
-                      value={ParentCategory}
-                      onChange={handleChangeParentCategory}
-                      label="Danh mục cha"
-                    >
-                      <MenuItem value="">
-                        <em>Chọn danh mục cha</em>
-                      </MenuItem>
-                      {dataCategories.map((category) => (
-                        <MenuItem key={category._id} value={category._id}>
-                          {category.name}
-                        </MenuItem>
-                      ))}
-                    </Select>
+      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Typography variant="h4">Tạo một Danh mục mới</Typography>
+      </Stack>
+      <Box justifyContent="center">
+        <Box sx={{ maxWidth: 'lg', p: 6, bgcolor: 'background.paper', color: 'text.primary', borderRadius: 2, boxShadow: 3, mt: 5 }}>
+          <Typography variant="h6" gutterBottom>Chi tiết</Typography>
+          <Typography variant="body2" color="text.secondary" gutterBottom>Tiêu đề, mô tả, hình ảnh...</Typography>
+          <Formik
+            initialValues={{ productName: '', description: '', parentCategory: '' }}
+            validationSchema={validationSchema}
+            onSubmit={async (values, { setSubmitting }) => {
+              const data = {
+                name: values.productName,
+                description: values.description,
+                slug: values.productName.toLowerCase().replace(/ /g, '-'),
+                parentId: values.parentCategory || null || "null",
+              };
+              try {
+                console.log(data);
+                await dispatch(createCategoryAsync({ data })).unwrap();
+                handleToast('success','Danh mục đã được tạo thành công!');
+              } catch (error) {
+                handleToast('error','Có lỗi xảy ra khi tạo danh mục.');
+              } finally {
+                setSubmitting(false);
+              }
+            }}
+          >
+            {({ handleSubmit }) => (
+              <Form onSubmit={handleSubmit}>
+                <Grid >
+                  <Grid item xs={12} sm={6}>
+                    <Field
+                      as={TextField}
+                      fullWidth
+                      id="productName"
+                      name="productName"
+                      label="Tên Danh mục"
+                      variant="outlined"
+                      placeholder="Tên danh mục"
+                      helperText={<ErrorMessage name="productName" component="div" className="error-message" />}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <InputLabel htmlFor="description">Mô tả</InputLabel>
+                    <Field name="description">
+                      {({ field, form }) => (
+                        <EditorContent
+                          {...field}
+                          value={field.value}
+                          onChange={(content) => {
+                            form.setFieldValue(field.name, content);
+                          }}
+                        />
+                      )}
+                    </Field>
                     <FormHelperText>
-                      <ErrorMessage name="parentCategory" component="div" className="error-message" />
+                      <ErrorMessage name="description" component="div" className="error-message" />
                     </FormHelperText>
-                  </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <InputLabel htmlFor="parentCategory">Danh mục cha</InputLabel>
+                    <FormControl fullWidth variant="outlined">
+                      <InputLabel htmlFor="parentCategory">Danh mục cha</InputLabel>
+                      <Select
+                        labelId="parentCategory-label"
+                        id="parentCategory"
+                        value={ParentCategory}
+                        onChange={handleChangeParentCategory}
+                        label="Danh mục cha"
+                      >
+                        <MenuItem value="">
+                          <em>Chọn danh mục cha</em>
+                        </MenuItem>
+                        {dataCategories.map((category) => (
+                          <MenuItem key={category._id} value={category._id}>
+                            {category.name}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                      <FormHelperText>
+                        <ErrorMessage name="parentCategory" component="div" className="error-message" />
+                      </FormHelperText>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <InfoBox title="Hình ảnh">
+                      <ImageDropZone handleUpload={handleChangeUploadImg} />
+                    </InfoBox>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button type="submit" variant="contained" sx={{ backgroundColor: '#1C252E', color: 'foreground-foreground', py: 2, px: 4, borderRadius: '8px', '&:hover': { backgroundColor: '#1C252E', opacity: 0.8 } }}>
+                      Tạo Danh mục
+                    </Button>
+                  </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                  <InfoBox title="Hình ảnh">
-                    <ImageDropZone handleUpload={handleChangeUploadImg} />
-                  </InfoBox>
-                </Grid>
-                <Grid item xs={12}>
-                  <Button type="submit" variant="contained" sx={{ backgroundColor: '#1C252E', color: 'foreground-foreground', py: 2, px: 4, borderRadius: '8px', '&:hover': { backgroundColor: '#1C252E', opacity: 0.8 } }}>
-                    Tạo Danh mục
-                  </Button>
-                </Grid>
-              </Grid>
-            </Form>
-          )}
-        </Formik>
+              </Form>
+            )}
+          </Formik>
+        </Box>
       </Box>
-    </Box>
-  </Container>
+    </Container>
   );
 };
 
