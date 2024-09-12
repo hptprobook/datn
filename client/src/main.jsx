3d7f87eabcd24000721622b499039143af26e044
Phan Thanh Hóa <hptprobook@gmail.com>
2024-09-12 14:00:59 +0700

    mock data edit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,14 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <BrowserRouter basename="/">
-    <App />
+    <QueryClientProvider client={QueryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>
   // </React.StrictMode>
 );
