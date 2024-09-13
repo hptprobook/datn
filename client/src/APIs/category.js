b8902a4849e1d9ae222b94cc6af625aced3b6363
Phan Thanh Hóa <hptprobook@gmail.com>
2024-09-13 10:01:52 +0700

    cấu hình tanstack query

---
+++ client/src/APIs/category.js
@@ -0,0 +1,13 @@
+import request from '~/config/axiosConfig';
+
+export const getAllCategory = async () => {
+  return await request.get('/categories').then((res) => res.data);
+};
+
+export const getCategoryById = async (categoryId) => {
+  return await request.get(`/categories/${categoryId}`).then((res) => res.data);
+};
+
+export const getCategoryBySlug = async (slug) => {
+  return await request.get(`/categories/slug/${slug}`).then((res) => res.data);
+};
