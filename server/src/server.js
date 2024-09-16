c5b29d42cfddf04cfbb42beb0376df0a54a8b0e9
tamle23122004 <tamle23122004@gmail.com>
2024-09-16 08:09:35 +0700

    fix payload large upload

--- server/src/server.js
+++ server/src/server.js
@@ -16,18 +16,18 @@ import path from 'path';
 const START_SERVER = () => {
   const app = express();
   const server = http.createServer(app);
-
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
   app.use(cookieParser());
   app.use(cors());
+  app.use(express.json());
   // app.use(
   //   cors({
   //     origin: 'http://localhost:5173',
   //     credentials: true,
   //   })
   // );
-  app.use(express.json());
-  app.use(express.json({ limit: '50mb' }));
-  app.use(express.urlencoded({ extended: true }));
+
   app.use(errorHandlingMiddleware);
 
   // Serve static files from the 'src/public/imgs' directory
