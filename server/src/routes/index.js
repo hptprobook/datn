d23feecc696e86d8e4d189679ae866ca6c3d6248
Nguyễn Văn Tín <126067526+nvtin1104@users.noreply.github.com>
2024-10-15 15:17:06 +0700

    create route staff

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -26,12 +26,13 @@ import { couponApi } from './couponRouter';
 import { addressApi } from './addressRouter';
 import { webBannerApi } from './routeConfigs/webBannerRouter';
 import { blogApi } from './blogRouter';
+import { staffsApi } from './staffsRouter';
 const Router = express.Router();
 
 Router.use('/auth', authApi);
 Router.use('/users', usersApi);
 Router.use('/categories', categoriesApi);
-
+Router.use('/staffs', staffsApi);
 Router.use('/carts', cartsApi);
 Router.use('/orders', ordersApi);
 Router.use('/reviews', reviewsApi);
