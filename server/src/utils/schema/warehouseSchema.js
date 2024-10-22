96d561421953ae0e25db3c613a5c5f1dce300992
ccxtncntus <ccxtncn00@gmail.com>
2024-10-22 14:13:35 +0700

    update order not login, Kho 50%, login by Social

--- server/src/utils/schema/warehouseSchema.js
+++ server/src/utils/schema/warehouseSchema.js
@@ -1,36 +1,55 @@
 import Joi from 'joi';
 
 export const SAVE_WAREHOUSES = Joi.object({
-    name: Joi.string().trim().min(1).required().messages({
+    name: Joi.string().trim().required().messages({
         'string.empty': 'Tên không được để trống',
         'any.required': 'Tên là bắt buộc',
     }),
-    location: Joi.string().trim().min(1).required().messages({
+    location: Joi.string().trim().required().messages({
         'string.empty': 'Địa chỉ không được để trống',
         'any.required': 'Địa chỉ là bắt buộc',
     }),
-    status: Joi.boolean().required().messages({
-        'any.required': 'Trạng thái là bắt buộc',
-    }),
-    capacity: Joi.number().integer().required().messages({
-        'string.empty': 'Sức chứa không được để trống',
-        'any.required': 'Sức chứa là bắt buộc',
-    }),
-    currentInventory: Joi.number().integer().default(0),
+    lat: Joi.number().default(null),
+    long: Joi.number().default(null),
+    province_id: Joi.number().integer(),
+    district_id: Joi.number().integer(),
+    ward_id: Joi.string(),
+    status: Joi.string()
+        .valid('active', 'close', 'full')
+        .default('active')
+        .messages({
+            'any.base':
+                'Trạng thái của kho gồm "Hoạt động", "Đóng cửa", "Đầy kho"',
+        }),
+    capacity: Joi.number().integer().default(10000),
+    currentQuantity: Joi.number().integer().default(0),
     createdAt: Joi.date().timestamp('javascript').default(Date.now),
     updatedAt: Joi.date().timestamp('javascript').default(Date.now),
 });
+
 export const UPDATE_WAREHOUSES = Joi.object({
-    name: Joi.string().trim().min(1).messages({
+    name: Joi.string().trim().required().messages({
         'string.empty': 'Tên không được để trống',
+        'any.required': 'Tên là bắt buộc',
     }),
-    location: Joi.string().trim().min(1).messages({
+    location: Joi.string().trim().required().messages({
         'string.empty': 'Địa chỉ không được để trống',
+        'any.required': 'Địa chỉ là bắt buộc',
     }),
-    status: Joi.boolean(),
-    capacity: Joi.number().integer().required().messages({
-        'string.empty': 'Sức chứa không được để trống',
-    }),
-    currentInventory: Joi.number().integer().default(0),
+    lat: Joi.number().default(null),
+    long: Joi.number().default(null),
+    province_id: Joi.number().integer(),
+    district_id: Joi.number().integer(),
+    ward_id: Joi.string(),
+    status: Joi.string()
+        .valid('active', 'close', 'full')
+        .default('active')
+        .messages({
+            'any.base':
+                'Trạng thái của kho gồm "Hoạt động", "Đóng cửa", "Đầy kho"',
+        }),
+    capacity: Joi.number().integer().default(10000),
+    currentQuantity: Joi.number().integer().default(0),
+    createdAt: Joi.date().timestamp('javascript').default(Date.now),
     updatedAt: Joi.date().timestamp('javascript').default(Date.now),
 });
