8d26eff1f5d7ac5d735f54a86c7c2933be8f3b38
Nguyễn Văn Tín <126067526+nvtin1104@users.noreply.github.com>
2024-10-29 17:07:55 +0700

    sửa logo

--- dashboard/src/components/logo/logo.jsx
+++ dashboard/src/components/logo/logo.jsx
@@ -2,29 +2,39 @@ import { useState, forwardRef } from 'react';
 import PropTypes from 'prop-types';
 
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 import { useTheme } from '@mui/material/styles';
 
-import { RouterLink } from 'src/routes/components';
-import { ListItem, List, ListItemButton, ListItemIcon, ListItemText, Popper } from '@mui/material';
+import { List, Popper, ListItem, ListItemIcon, ListItemText, ListItemButton } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import Iconify from '../iconify';
+
 // ----------------------------------------------------------------------
 
 const Logo = forwardRef(({ sx, ...other }, ref) => {
   const theme = useTheme();
+  const clientUrl = import.meta.env.VITE_REACT_CLIENT_URL;
+  console.log(clientUrl);
 
   const url = theme.palette.primary.light
-    ? 'assets/images/logo/dark.png'
-    : 'assets/images/logo/light.png';
+    ? '/assets/images/logo/dark.png'
+    : '/assets/images/logo/light.png';
   const [anchorEl, setAnchorEl] = useState(null);
 
   const handleClick = (event) => {
     setAnchorEl(anchorEl ? null : event.currentTarget);
   };
+  const navigate = useNavigate();
 
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popper' : undefined;
-
+  const handleNavigate = (u, o) => {
+    setAnchorEl(null);
+    if (o === false) {
+      window.open(u, '_blank');
+    } else {
+      navigate(u);
+    }
+  };
   // -------------------------------------------------------
 
   return (
@@ -47,23 +57,66 @@ const Logo = forwardRef(({ sx, ...other }, ref) => {
           zIndex: 9999,
         }}
       >
-        <Box sx={{ p: 1, bgcolor: 'background.paper' }}>
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            boxShadow: 24,
+            borderRadius: 1,
+          }}
+        >
           <nav aria-label="main mailbox folders">
             <List>
               <ListItem disablePadding>
-                <ListItemButton>
+                <ListItemButton
+                  sx={{
+                    p: '0 12px',
+                  }}
+                  onClick={() => handleNavigate(clientUrl, true)}
+                >
                   <ListItemIcon>
-                    <Iconify icon="eva:home-fill" />
+                    <Iconify icon="mdi:web" />
                   </ListItemIcon>
-                  <ListItemText primary="Inbox" />
+                  <ListItemText primary="Website" />
                 </ListItemButton>
               </ListItem>
               <ListItem disablePadding>
-                <ListItemButton>
+                <ListItemButton
+                  sx={{
+                    p: '0 12px',
+                  }}
+                  onClick={() => handleNavigate('/')}
+                >
                   <ListItemIcon>
                     <Iconify icon="eva:home-fill" />
                   </ListItemIcon>
-                  <ListItemText primary="Drafts" />
+                  <ListItemText primary="Trang chủ" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton
+                  sx={{
+                    p: '0 12px',
+                  }}
+                  onClick={() => handleNavigate('/admin')}
+                >
+                  <ListItemIcon>
+                    <Iconify icon="fa6-solid:warehouse" />
+                  </ListItemIcon>
+                  <ListItemText primary="Kho" />
+                </ListItemButton>
+              </ListItem>
+
+              <ListItem disablePadding>
+                <ListItemButton
+                  sx={{
+                    p: '0 12px',
+                  }}
+                  onClick={() => handleNavigate('/pos')}
+                >
+                  <ListItemIcon>
+                    <Iconify icon="mdi:network-pos" />
+                  </ListItemIcon>
+                  <ListItemText primary="POS" />
                 </ListItemButton>
               </ListItem>
             </List>
